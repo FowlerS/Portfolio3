5d75bbf452ea9a8c6ca7f8618e68b128318dd9ad
Shamon Fowler <shamonlfjr@gmail.com>
2015-01-12 18:25:30 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -13,7 +13,23 @@ $("document").ready(function(){
     
     $("#0").css("color", "red");
     
+    $("#0").css("border", "solid");
+    
+    $("#0").css("border-color", "white");
+    
+    $("#3").css("border", "solid");
+    
+    $("#3").css("border-color", "white");
+    
+    $("#4").css("border", "solid");
+    
+    $("#4").css("border-color", "white");
+    
     $("#0").css("text-align", "center");
+    
+    $("#3").css("text-align", "center");
+    
+    $("#4").css("text-align", "center");
     
     $("#1").css("text-align", "center");
     
@@ -21,8 +37,42 @@ $("document").ready(function(){
     
     $("p").css("font-size", "30px");
     
+    $("#0").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $("#3").bind('click', backgroundChange);
+    
+    $("#4").bind('click', backgroundChangeBack);
+    
 });
 
+function backgroundChange(){
+    $(".00").css("background-color", "white");
+    
+    $("#0 ~ *").css("color", "blue");
+    
+    $("#0").css("border-color", "black");
+    
+    $("#3").css("border-color", "black");
+    
+    $("#4").css("border-color", "black");
+}
+
+function backgroundChangeBack(){
+    $(".00").css("background-color", "black");
+    
+    $("#0 ~ *").css("color", "red");
+    
+    $("#0").css("border-color", "white");
+    
+    $("#3").css("border-color", "white");
+    
+    $("#4").css("border-color", "white");
+}
+
 function mouseOverMe(){
-//    $("#0").bind('click');
+    $("#0").html("Portfolio 3");
+}
+
+function mouseOutMe(){
+    $("#0").html("Hello, my name is Shamon Fowler");
 }
