7d894f48342bda2079a7674987e03e533915b471
Shamon Fowler <shamonlfjr@gmail.com>
2015-01-14 10:57:30 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -17,60 +17,68 @@ $("document").ready(function(){
     
     $("#0").css("border-color", "white");
     
+    $("#2").css("border", "solid");
+    
+    $("#2").css("border-color", "white");
+    
     $("#3").css("border", "solid");
     
     $("#3").css("border-color", "white");
     
-    $("#4").css("border", "solid");
-    
-    $("#4").css("border-color", "white");
-    
     $("#0").css("text-align", "center");
+    
+    $("#2").css("text-align", "center");
     
     $("#3").css("text-align", "center");
     
-    $("#4").css("text-align", "center");
-    
     $("#1").css("text-align", "center");
     
-    $("#2").css("text-align", "left");
+    $("#Facts").css("text-align", "left");
+    
+    $("#Interest").css("text-align", "left");
     
     $("p").css("font-size", "30px");
     
     $("#0").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
     
-    $("#3").bind('click', backgroundChange);
+    $("#2").bind('click', backgroundChange);
     
-    $("#4").bind('click', backgroundChangeBack);
+    $("#3").bind('click', backgroundChangeBack);
     
 });
 
+//These two functions changes the background color in addition to the text and border color.//
+
 function backgroundChange(){
     $(".00").css("background-color", "white");
+    
+    $("#0").css("color", "blue");
     
     $("#0 ~ *").css("color", "blue");
     
     $("#0").css("border-color", "black");
     
-    $("#3").css("border-color", "black");
+    $("#2").css("border-color", "black");
     
-    $("#4").css("border-color", "black");
+    $("#3").css("border-color", "black");
 }
 
 function backgroundChangeBack(){
     $(".00").css("background-color", "black");
     
+    $("#0").css("color", "red");
+    
     $("#0 ~ *").css("color", "red");
     
     $("#0").css("border-color", "white");
     
-    $("#3").css("border-color", "white");
+    $("#2").css("border-color", "white");
     
-    $("#4").css("border-color", "white");
+    $("#3").css("border-color", "white");
 }
 
 function mouseOverMe(){
-    $("#0").html("Portfolio 3");
+    $("#0").html("Aka CrimsonSlayer96 on xbox one");
 }
 
 function mouseOutMe(){
